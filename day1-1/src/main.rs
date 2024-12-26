58f1a35426fd6f2899d54c5b6bdaa795d53011f8
Fred Martin <fredm@alum.mit.edu>
2024-12-26 13:48:56 -0600

    Create main.rs

---
+++ day1-1/src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+
+fn main() {
+    let mut xs: Vec<i32> = vec![];
+    let mut ys: Vec<i32> = vec![];
+    
+    loop {
+        let mut in_str = String::new();
+        let num_read = io::stdin().read_line(&mut in_str)
+            .expect("Failed to read line");
+
+        if num_read == 0 {
+            break;
+        }
+
+        let mut split = in_str.split("   ");
+
+        let x: i32 = split.next().expect("first split failed")
+            .parse().expect("first not an int");
+        xs.push(x);
+
+
+        let y: i32 = split.next().expect("second split failed").trim()
+            .parse().expect("second not an int");
+        ys.push(y);
+
+    }
+
+    xs.sort();
+    ys.sort();
+
+    let mut sum = 0i32;
+    
+    for (x, y) in xs.iter().zip(ys.iter()) {
+        sum = sum + (x - y).abs();
+    }
+
+    println!("{}", sum);
+}
